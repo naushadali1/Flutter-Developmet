112117b2a805ee59169ccacdaa57b64cb1f294ce
Naushad Ali <nashadali585@gmail.com>
2023-02-04 02:20:24 +0500

    Refactor Code
    refactor with widget class

--- Login UI/login_ui/flutter_application_1/lib/main.dart
+++ Login UI/login_ui/flutter_application_1/lib/main.dart
@@ -27,45 +27,12 @@ class MyApp extends StatelessWidget {
             ),
             child: SingleChildScrollView(
               child: Column(
+                // ignore: prefer_const_literals_to_create_immutables
                 children: [
                   const SizedBox(
                     height: 20,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Image(
-                        image: AssetImage('images/logo.png'),
-                        height: 50,
-                        width: 50,
-                      ),
-                      const SizedBox(width: 20),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Center(
-                            child: Text(
-                              "Login Page",
-                              style: TextStyle(
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Rubik Medium'),
-                            ),
-                          ),
-                          Center(
-                            child: Text(
-                              "Welcome",
-                              style: TextStyle(
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Rubik Medium',
-                                  color: Colors.orange),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                  Rows(),
                   const SizedBox(
                     height: 16,
                   ),
@@ -91,50 +58,11 @@ class MyApp extends StatelessWidget {
                   SizedBox(
                     height: 7,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 9),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Email',
-
-                        // ignore: prefer_const_constructors
-                        prefixIcon: Icon(
-                          Icons.email_rounded,
-                          color: Colors.black,
-                        ),
-                        filled: true,
-                        fillColor: const Color.fromARGB(255, 208, 214, 217),
-                        border: OutlineInputBorder(
-                            borderSide: const BorderSide(color: Colors.black),
-                            borderRadius: BorderRadius.circular(10)),
-                      ),
-                    ),
-                  ),
+                  Email(),
                   const SizedBox(
                     height: 10,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 9),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        // ignore: prefer_const_constructors
-                        prefixIcon: Icon(
-                          Icons.password_sharp,
-                          color: Colors.black,
-                        ),
-                        suffixIcon: const Icon(
-                          Icons.visibility_off_outlined,
-                          color: Colors.black,
-                        ),
-                        filled: true,
-                        fillColor: const Color.fromARGB(255, 208, 214, 217),
-                        border: OutlineInputBorder(
-                            borderSide: const BorderSide(color: Colors.black),
-                            borderRadius: BorderRadius.circular(10)),
-                      ),
-                    ),
-                  ),
+                  const Passwords(),
 
                   const SizedBox(
                     height: 4,
@@ -145,12 +73,7 @@ class MyApp extends StatelessWidget {
                   const SizedBox(
                     height: 7,
                   ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    // ignore: prefer_const_constructors
-                    style: ButtonStyle(),
-                    child: const Text("Login"),
-                  ),
+                  Buttons(),
                   // ignore: prefer_const_constructors
                   SizedBox(
                     height: 10,
@@ -168,6 +91,128 @@ class MyApp extends StatelessWidget {
           )),
         ),
       ),
+    );
+  }
+}
+
+class Passwords extends StatelessWidget {
+  const Passwords({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 9),
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: 'Password',
+          // ignore: prefer_const_constructors
+          prefixIcon: Icon(
+            Icons.password_sharp,
+            color: Colors.black,
+          ),
+          suffixIcon: const Icon(
+            Icons.visibility_off_outlined,
+            color: Colors.black,
+          ),
+          filled: true,
+          fillColor: const Color.fromARGB(255, 208, 214, 217),
+          border: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.black),
+              borderRadius: BorderRadius.circular(10)),
+        ),
+      ),
+    );
+  }
+}
+
+class Email extends StatelessWidget {
+  const Email({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 9),
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: 'Email',
+
+          // ignore: prefer_const_constructors
+          prefixIcon: Icon(
+            Icons.email_rounded,
+            color: Colors.black,
+          ),
+          filled: true,
+          fillColor: const Color.fromARGB(255, 208, 214, 217),
+          border: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.black),
+              borderRadius: BorderRadius.circular(10)),
+        ),
+      ),
+    );
+  }
+}
+
+class Rows extends StatelessWidget {
+  const Rows({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Image(
+          image: AssetImage('images/logo.png'),
+          height: 50,
+          width: 50,
+        ),
+        const SizedBox(width: 20),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Center(
+              child: Text(
+                "Login Page",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Rubik Medium'),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Welcome",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Rubik Medium',
+                    color: Colors.orange),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class Buttons extends StatelessWidget {
+  const Buttons({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      // ignore: prefer_const_constructors
+      style: ButtonStyle(),
+      child: const Text("Login"),
     );
   }
 }
